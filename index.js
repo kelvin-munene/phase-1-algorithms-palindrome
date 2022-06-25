48df519f8a21e59b7b3d3c48cf6d3a59b5b069d8
kevin <kmunene858@gmail.com>
2022-06-25 11:58:31 +0300

    done

--- index.js
+++ index.js
@@ -1,42 +1,43 @@
-
+function reverse(word) {
+  // 'abc' => 'cba'
+  // const wordArray = word.split("");
+  // const reversedWordArray = wordArray.reverse();
+  // const reversedWord = reversedWordArray.join("");
+  // return reversedWord;
+  return word.split("").reverse().join("");
+}
 
 function isPalindrome(word) {
-  // let clonedWord = []
-  // for(let i=0; i<word.length; i++){
-  //   clonedWord.push(word[i])
-  // }
-  // let reversedWord = [...clonedWord]
-  let clonedWord = [...word]
-  let reversedWord = clonedWord.reverse().join('')     //tried avoiding looping backwards again
-  // clonedWord = clonedWord.join('')      //redundant
-  // reversedWord = reversedWord.join('')     //redundant
-  return reversedWord === word;
+  // Write your algorithm here
+  const reversedWord = reverse(word);
+
+  return word === reversedWord;
 }
 
 /* 
+  Add your pseudocode here
+  that means if the word is the same as the word in reverse, I should return true.
   
-  Convert string to array
-  Loop forward and backward allocating return value to different variables
-  Convert the variables back to string for later comparisons
-  Compare the said variables and return true if same and false if not
+  reverse the input string
+  if the input is the same as the reversed input
+    return true
+  else
+    return false
 */
 
 /*
-  Add written explanation of your solution here
-  Point to note, may not work on disregarding whitespace and symbols
-*/
-
-console.log(isPalindrome('racecar'))
-
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
-
-  console.log("");
+@@ -20,6 +39,16 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
-}
 
-module.exports = isPalindrome;
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome(""));
+}
